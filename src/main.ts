af9a63bd4b0442c65792d28d90782ac622e1cd01
Henry Lee <gbvrcx@gmail.com>
2022-04-20 11:38:44 +0000

    style: 4 spaces instead of tab

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'Soodle'
-	}
+    target: document.body,
+    props: {
+        name: 'Soodle'
+    }
 });
 
 export default app;
