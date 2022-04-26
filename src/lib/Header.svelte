497dc75e9185b5b5434f5ac8775fbc2183611060
Henry Lee <gbvrcx@gmail.com>
2022-04-26 13:08:52 +0000

    feat: link to game page

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import { GAME_TITLE } from './config';
+	import { GAME_TITLE, GAME_URL } from './config';
 	import Setting from '$lib/Modals/Setting.svelte';
 	import Stats from '$lib/Modals/Stats.svelte';
 </script>
 
 <header>
-	<div class='header'>
-		<h1>{GAME_TITLE}</h1>
+	<div class="header">
+		<h1><a href={GAME_URL}>{GAME_TITLE}</a></h1>
 		<div class="modals">
 			<Stats />
 			<Setting />
@@ -24,10 +24,99 @@
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
-		width:30em;
+		width: 30em;
 	}
 
 	h1 {
 		font-size: x-large;
+	}
+
+	h1 > a {
+		color: black;
+		text-decoration: none;
+	}
+
+	h1 > a:hover {
+		-webkit-animation: rainbow 1s infinite;
+		animation: rainbow 1s infinite;
+	}
+
+	@keyframes rainbow {
+		0%,
+		100% {
+			color: #ff0000;
+		}
+		8% {
+			color: #ff7f00;
+		}
+		17% {
+			color: #ffff00;
+		}
+		25% {
+			color: #7fff00;
+		}
+		33% {
+			color: #00ff00;
+		}
+		42% {
+			color: #00ff7f;
+		}
+		50% {
+			color: #00ffff;
+		}
+		58% {
+			color: #007fff;
+		}
+		67% {
+			color: #0000ff;
+		}
+		75% {
+			color: #7f00ff;
+		}
+		83% {
+			color: #ff00ff;
+		}
+		92% {
+			color: #ff007f;
+		}
+	}
+	@keyframes rainbow {
+		0%,
+		100% {
+			color: #ff0000;
+		}
+		8% {
+			color: #ff7f00;
+		}
+		17% {
+			color: #ffff00;
+		}
+		25% {
+			color: #7fff00;
+		}
+		33% {
+			color: #00ff00;
+		}
+		42% {
+			color: #00ff7f;
+		}
+		50% {
+			color: #00ffff;
+		}
+		58% {
+			color: #007fff;
+		}
+		67% {
+			color: #0000ff;
+		}
+		75% {
+			color: #7f00ff;
+		}
+		83% {
+			color: #ff00ff;
+		}
+		92% {
+			color: #ff007f;
+		}
 	}
 </style>
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,3 @@
 export const GAME_TITLE = '수들';
+export const GAME_URL = '/#';
 export const GITHUB_ADDRESS = 'github.com/heyumteam/soodle';
