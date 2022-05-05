9a8ade6657cd2c120d7d50f764afe1631209614d
Henry Lee <gbvrcx@gmail.com>
2022-05-05 01:58:42 +0000

    refactor: update configs

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,10 @@
-export const GAME_TITLE = '수들';
+// url settings
 export const GAME_URL = '/#';
 export const GITHUB_ADDRESS = 'github.com/heyumteam/soodle';
+
+// game settings
 export const MAX_TRIAL = 6;
-export const TOAST_TIMEOUT = 2000;
 export const NUM_WORDS = 2;
+
+// environment settings
+export const TOAST_TIMEOUT = 2000;
