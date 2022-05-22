b62c42d8461107d68ede86bebf8fcc61aa8d8e0c
mook <dlsanr0111@gmail.com>
2022-05-22 09:25:04 +0900

    사전에 있나없나

--- src/lib/secret/word.ts
+++ src/lib/secret/word.ts
@@ -1,6 +1,9 @@
 import type { Char } from '$lib/type';
 import WORD_LIST from './word.json';
 
-export const isInWordList = (chars: Char[]) => {
+export const isInWordList = (chars: string) => {
+	if(WORD_LIST.includes(chars))
+	return true;
+	else 
 	return false;
 };
