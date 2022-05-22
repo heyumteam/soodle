ee98a169921ee5075cae88a9c2eac79fe48c0402
Henry Lee <gbvrcx@gmail.com>
2022-05-22 01:21:17 +0000

    fix: not revealing text in transparant mode animation

--- src/lib/component/grid/Cell.svelte
+++ src/lib/component/grid/Cell.svelte
@@ -25,8 +25,11 @@
 	class:transparent-mode={$transparentMode}
 	class:not-in-correct-row={!isInCorrectRow}
 	class:in-correct-row={isInCorrectRow}
-	style="--duration: {animationDuration}s; --delay: {animationDelay}s; --pop-delay: {popDelay +
-		animationDelay}s;"
+	style="--duration: {animationDuration}s; 
+		--delay: {animationDelay}s;
+		--pop-delay: {popDelay + animationDelay}s;
+		--text-in-cell-color: {$transparentMode ? 'transparent' : 'var(--text-color)'};
+		--text-in-checked-cell-color: {$transparentMode ? 'transparent' : 'var(--checked-text-color)'}"
 >
 	{char ?? ''}
 </div>
@@ -49,12 +52,12 @@
 	}
 
 	div.char-input {
-		color: var(--text-color);
+		color: var(--text-in-cell-color);
 		border: 0.1em solid grey;
 	}
 
 	div.checked {
-		color: var(--checked-text-color);
+		color: var(--text-in-checked-cell-color);
 		border: 0.1em solid transparent;
 	}
 
@@ -89,13 +92,13 @@
 		0% {
 			transform: rotateX(0deg);
 			border: 0.1em solid grey;
-			color: var(--text-color);
+			color: var(--text-in-cell-color);
 			background-color: transparent;
 		}
 		50% {
 			transform: rotateX(90deg);
 			border: 0.1em solid grey;
-			color: var(--text-color);
+			color: var(--text-in-cell-color);
 			background-color: transparent;
 		}
 		100% {
