2216590b8cfd006884585f5947092ce9595a81ba
Henry Lee <gbvrcx@gmail.com>
2022-05-22 00:34:15 +0000

    fix: check word in word list

--- src/lib/secret/word.ts
+++ src/lib/secret/word.ts
@@ -1,9 +1,7 @@
 import type { Char } from '$lib/type';
 import WORD_LIST from './word.json';
 
-export const isInWordList = (chars: string) => {
-	if(WORD_LIST.includes(chars))
-	return true;
-	else 
-	return false;
+export const isInWordList = (chars: Char[]) => {
+	const word = chars.join('');
+	return WORD_LIST.includes(word.toLowerCase());
 };
