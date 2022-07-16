92e8b2609758d93e34179a0877d5483c9cac6b83
Henry Lee <gbvrcx@gmail.com>
2022-07-16 12:00:48 +0000

    chore: upgrade sveltekit version

--- svelte.config.js
+++ svelte.config.js
@@ -9,14 +9,10 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
+
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
-		},
-		vite: {
-			define: {
-				__version__: JSON.stringify(process.env.npm_package_version)
-			}
 		}
 	}
 };
